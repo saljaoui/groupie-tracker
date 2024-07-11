6e95a0ecff1b369ac74bcf818be10887a17b2c46
soufian <161062883+saljaoui@users.noreply.github.com>
2024-07-11 09:43:43 +0100

    Create main.go
    h

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Artists struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
+
+func main() {
+	http.HandleFunc("/artists", artistsHandler)
+	http.HandleFunc("/relation", relationHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	fmt.Println("Server listening on port 8080...")
+	fmt.Println("http://localhost:8080/artists")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func artistsHandler(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	var artists []Artists
+	if err := json.NewDecoder(resp.Body).Decode(&artists); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Execute HTML template
+	tmpl := template.Must(template.ParseFiles("index.html")) // Adjust path as needed
+	err = tmpl.Execute(w, artists)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func relationHandler(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/relation")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+	var artists []Artists
+	if err := json.NewDecoder(resp.Body).Decode(&artists); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Execute HTML template
+	tmpl := template.Must(template.ParseFiles("index.html")) // Adjust path as needed
+	err = tmpl.Execute(w, artists)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
